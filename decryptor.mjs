c1ea8d2d4a4e4635be9fc99a196e0a4668ee345e
Paul <devnoname120@gmail.com>
2023-07-18 20:24:01 +0200

    Add segments downloading and decryption

--- decryptor.mjs
+++ decryptor.mjs
@@ -4,33 +4,50 @@ import * as aes from "aes-cross";
 
 $.verbose = false;
 
-const aes_key_raw = await fs.readFile('books/BwCMEAAAQBAJ/aes_key.bin');
+const FETCH_OPTIONS = {};
+
+const BOOK_ID = 'BwCMEAAAQBAJ'; // Found in the URL of the book page. For example: BwCMEAAAQBAJ
+const GOOGLE_PAGE_DOWNLOAD_PACER = 100; // Wait between requests to reduce risk of getting flagged for abuse. The official frontend sequentially spams the segments API without wait so it should be safe to put a crazy number here.
+
+const aes_key_raw = await fs.readFile(`books/${BOOK_ID}/aes_key.bin`);
 const aes_key = new Uint8Array(aes_key_raw);
 console.log(`[aes_key]: ${aes_key}\n`);
 console.log(aes_key);
 
-const dir_path = '/Users/paul/Response_Body.folder';
+const manifest = JSON.parse(await fs.readFile(`books/${BOOK_ID}/manifest.json`));
 
-const names = await fs.readdir(dir_path);
-// const names = ['[97] Response - play.google.com_books_volumes_BwCMEAAAQBAJ_content_segment'];
+const total = manifest.segment.length;
+const segment_files = [];
 
-for (const name of names) {
-  if (!name.includes('content_segment') || name.includes('.dec')) {
-    continue;
-  }
-  console.log(name)
-  const name_path = `${dir_path}/${name}`;
-  const buf_enc = await fs.readFile(name_path, 'utf8');
-  // console.log(buf_enc);
-  const buf = await decrypt(new Uint8Array(Buffer.from(buf_enc, 'base64')));
-  await fs.writeFile(`${name_path}.dec`, buf);
+log(`Starting to download ${total} segments…`);
 
+for (const segment of manifest.segment) {
+  // const p = `${i + 1}/${total}`;
+
+  const segment_url = 'https://play.google.com' + segment.link;
   try {
-    const parsed_buf = JSON.parse(buf);
-    await fs.writeFile(`${name_path}.dec.html`, parsed_buf.content);
+    console.log(`===> segment #${segment.order}: ${segment.label} (${segment.title})`);
+
+    const response = await fetch_segment(segment_url)
+    const buf_text = await response.text();
+    console.log(buf_text.length);
+
+    const buf_tmp = Buffer.from(buf_text, 'base64');
+    console.log(buf_tmp);
+
+    const buf = await decrypt(new Uint8Array(buf_tmp));
+
+    const filename = `${segment.order} - ${segment.title}.json`;
+    await fs.writeFile(filename, JSON.stringify(buf, null, 4));
+
+    segment_files.push(filename);
+
+    log(`Saved to ${filename} (url: ${segment_url})`);
   } catch (e) {
-    console.log(e)
+    err(`Error! Download or decrypt failed (url: ${segment_url}) failed with ${e.message}\n${e.stack}`);
   }
+
+  await sleep(GOOGLE_PAGE_DOWNLOAD_PACER); // Be gentle with Google Play Books
 }
 
 // const buf_enc = new Uint8Array(Buffer.from(b64_str, 'base64'));
@@ -64,7 +81,8 @@ async function decrypt(buf) {
     // console.log(dec_payload);
 
     try {
-      const dec_payload_json = JSON.stringify(JSON.parse(dec_payload_cut), null, 2);
+      console.log(dec_payload_cut);
+      const dec_payload_json = JSON.parse(dec_payload_cut);
       return dec_payload_json;
     } catch (e) {
       console.log(e);
@@ -73,4 +91,32 @@ async function decrypt(buf) {
   } catch (e) {
     console.log(e);
   }
+}
+
+function log(message) {
+  return console.log(`[${BOOK_ID}] ${message}`);
+}
+
+function info(message) {
+  return console.info(chalk.blue(`[${BOOK_ID}] ${message}`));
+}
+
+function success(message) {
+  return console.info(chalk.green(`[${BOOK_ID}] ${message}`));
+}
+
+function warn(message) {
+  return console.warn(chalk.yellow(`[${BOOK_ID}] ${message}`));
+}
+
+function err(message) {
+  return console.error(chalk.red(`[${BOOK_ID}] ${message}`));
+}
+
+async function fetch_segment(src) {
+  const segment_url = new URL(src);
+  segment_url.searchParams.set('enc_all', 1); // If we set it to 0 only the book content fields are encrypted in the JSON. Setting this to 1 encrypts the whole JSON which is easier because we can decrypt it in one go without wondering every time if a given field is encrypted or not.
+
+  console.log(`fetch: ${segment_url}`);
+  return fetch(segment_url, FETCH_OPTIONS);
 }
