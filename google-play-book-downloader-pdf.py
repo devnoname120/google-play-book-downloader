c98ba706987fb7e73f193d48f2afdf3e8bec7d22
Paul <devnoname120@gmail.com>
2024-06-29 22:22:45 +0200

    Ruff — Reformat everything
    All the things

--- google-play-book-downloader-pdf.py
+++ google-play-book-downloader-pdf.py
@@ -11,7 +11,7 @@ import logging
 from urllib.parse import urlparse, urlencode, urlunparse, parse_qs
 from Cryptodome.Cipher import AES
 
-BOOK_ID = ''  # Found in the URL of the book page. For example: BwCMEAAAQBAJ
+BOOK_ID = ""  # Found in the URL of the book page. For example: BwCMEAAAQBAJ
 GOOGLE_PAGE_DOWNLOAD_PACER = 0.5  # Wait between requests to reduce risk of getting flagged for abuse.
 
 # How to get this options object:
@@ -26,22 +26,21 @@ headers = {}
 
 # Configure logging
 logging.basicConfig(level=logging.INFO,
-                    format='[%(asctime)s] %(levelname)s: %(message)s')
+                    format="[%(asctime)s] %(levelname)s: %(message)s")
 
-logging.info(f'Script started for book id: {BOOK_ID}')
+logging.info(f"Script started for book id: {BOOK_ID}")
 
-book_dir = f'books/{BOOK_ID}'
+book_dir = f"books/{BOOK_ID}"
 os.makedirs(book_dir, exist_ok=True)
 os.chdir(book_dir)
 
-response = requests.get(f'https://play.google.com/books/reader?id={BOOK_ID}&hl=en',
-                        cookies=cookies,
-                        headers=headers)
+response = requests.get(f"https://play.google.com/books/reader?id={BOOK_ID}&hl=en",
+                        cookies=cookies, headers=headers)
 body = response.text
 
 
 def unescape_html(text):
-    return re.sub(r'&#(\d+);', lambda m: chr(int(m.group(1))), text)
+    return re.sub(r"&#(\d+);", lambda m: chr(int(m.group(1))), text)
 
 
 def extract_decryption_key(google_reader_body):
@@ -52,19 +51,21 @@ def extract_decryption_key(google_reader_body):
 
         key_data = key_search.group(1)
 
-        logging.info(f'Ciphered decryption key: {base64.b64decode(key_data)}')
+        logging.info(f"Ciphered decryption key: {base64.b64decode(key_data)}")
     except Exception as e:
         raise Exception(
-            f'Failed to extract the encoded decryption key from Play Book Reader\'s HTML body: {google_reader_body}') from e
+            f"Failed to extract the encoded decryption key from Play Book Reader's HTML body: {google_reader_body}"
+        ) from e
 
     return decipher_key(base64.b64decode(key_data, validate=True))
 
 
 def decipher_key(str_data):
-    groups = re.findall(r'(\D+\d)', str_data.decode())
+    groups = re.findall(r"(\D+\d)", str_data.decode())
     if len(groups) != 128:
         logging.warning(
-            f'Unexpected count of AES key groups. Expected: 128, got: {len(groups)}. Ignoring the error and continuing…')
+            f"Unexpected count of AES key groups. Expected: 128, got: {len(groups)}. Ignoring the error and continuing…"
+        )
 
     bitfield = [str(1 if s[int(s[-1])] == s[-2] else 0) for s in groups]
     shift = 64 % len(bitfield)
@@ -76,15 +77,15 @@ def decipher_key(str_data):
 
     key = []
     for pos in range(0, len(bitfield), 8):
-        bin_str = ''.join(reversed(bitfield[pos:pos + 8]))
+        bin_str = "".join(reversed(bitfield[pos: pos + 8]))
         key.append(int(bin_str, 2))
     return bytes(key)
 
 
 aes_key = extract_decryption_key(body)
-with open('aes_key.bin', 'wb') as key_file:
+with open("aes_key.bin", "wb") as key_file:
     key_file.write(aes_key)
-logging.info(f'Found AES decryption key: [{aes_key.hex()}]')
+logging.info(f"Found AES decryption key: [{aes_key.hex()}]")
 
 
 def extract_toc(google_reader_body):
@@ -93,62 +94,67 @@ def extract_toc(google_reader_body):
                              google_reader_body).group(1)
     except Exception as e:
         logging.warning(
-            f'Failed to extract the table of contents from the book\'s main page. Error: {e}')
+            f"Failed to extract the table of contents from the book's main page. Error: {e}")
         return None
 
     try:
         return json.loads(toc_data)
     except Exception as e:
         logging.warning(
-            f'Failed to parse the table of contents from the book\'s main page as JSON. Content: {toc_data} Error: {e}')
+            f"Failed to parse the table of contents from the book's main page as JSON. Content: {toc_data} Error: {e}"
+        )
         return None
 
 
 toc = extract_toc(body)
 
 manifest_response = requests.get(
-    f'https://play.google.com/books/volumes/{BOOK_ID}/manifest?hl=en&authuser=2&source=ge-web-app',
-    cookies=cookies, headers=headers)
+    f"https://play.google.com/books/volumes/{BOOK_ID}/manifest?hl=en&authuser=2&source=ge-web-app",
+    cookies=cookies,
+    headers=headers,
+)
 manifest_text = manifest_response.text
 manifest = json.loads(manifest_text)
-with open('manifest.json', 'w') as manifest_file:
+with open("manifest.json", "w") as manifest_file:
     json.dump(manifest, manifest_file, indent=4)
 
 if not toc:
-    toc = manifest.get('toc_entry')
+    toc = manifest.get("toc_entry")
     if toc:
         logging.warning(
-            "Using the table of contents from the manifest as a fallback. Note that it's inferior because everything is flattened to the top level instead of having subchapters")
+            "Using the table of contents from the manifest as a fallback. Note that it's inferior because everything is flattened to the top level instead of having subchapters"
+        )
     else:
         logging.error("Error! Couldn't find the table of contents in the book manifest")
 
 if toc:
-    with open('toc.json', 'w') as toc_file:
+    with open("toc.json", "w") as toc_file:
         json.dump(toc, toc_file, indent=4)
     logging.info("Extracted the table of contents to toc.json")
 
     try:
-        human_toc = '\n'.join(
+        human_toc = "\n".join(
             f"{'    ' * t['depth']}{unescape_html(t['label'])} ........".ljust(80,
-                                                                               '.') + f" p.{t['page_index'] + 1}"
+                                                                               ".") + f" p.{t['page_index'] + 1}"
             for t in toc
         )
-        with open('toc.txt', 'w') as human_toc_file:
+        with open("toc.txt", "w") as human_toc_file:
             human_toc_file.write(human_toc)
         logging.info("Wrote human-readable table of contents to toc.txt")
     except Exception as e:
         logging.warning(
             f"Warning: Couldn't produce a human-readable table of contents:\n{e}")
 
-missing_pages = [p['pid'] for p in manifest['page'] if
-                 not p.get('src') or not isinstance(p['src'], str)]
+missing_pages = [p["pid"] for p in manifest["page"] if
+                 not p.get("src") or not isinstance(p["src"], str)]
 missing = len(missing_pages)
-total = len(manifest['page'])
+total = len(manifest["page"])
 
 if missing != 0:
     missing_percent = f"{(missing / total):.2%}"
     logging.error(
-        f"Error! Couldn't find a download link for {missing} pages ({missing_percent} missing, total: {total} pages). Make sure that the FETCH_OPTIONS object is valid and that you own the book. List of missing pages: [{', '.join(map(str, missing_pages))}]")
+        f"Error! Couldn't find a download link for {missing} pages ({missing_percent} missing, total: {total} pages). Make sure that the FETCH_OPTIONS object is valid and that you own the book. List of missing pages: [{', '.join(map(str, missing_pages))}]"
+    )
 
 page_files = []
 
@@ -158,21 +164,23 @@ logging.info(f"Starting to download {total} pages…")
 def download_page(src):
     url_parts = list(urlparse(src))
     query = parse_qs(url_parts[4])
-    query.update({
-        'w': ['10000'],
-        # Arbitrarily high number to make sure that we retrieve the highest resolution
-        'h': ['10000'],
-        'zoom': ['3'],  # Zoom values 1 and 2 are for thumbnails (degraded quality)
-        'enc_all': ['1'],
-        'img': ['1']
-    })
+    query.update(
+        {
+            "w": ["10000"],
+            # Arbitrarily high number to make sure that we retrieve the highest resolution
+            "h": ["10000"],
+            "zoom": ["3"],  # Zoom values 1 and 2 are for thumbnails (degraded quality)
+            "enc_all": ["1"],
+            "img": ["1"],
+        }
+    )
     url_parts[4] = urlencode(query, doseq=True)
     page_url = urlunparse(url_parts)
     logging.info(f"Downloading url: {page_url}")
 
     response = requests.get(page_url, cookies=cookies, headers=headers)
 
-    mimeType = response.headers.get('content-type')
+    mimeType = response.headers.get("content-type")
     buffer = response.content
 
     return mimeType, buffer
@@ -190,24 +198,24 @@ def decrypt(buf):
 
 def mime_to_ext(mime):
     lookup = {
-        'image/png': 'png',
-        'image/jpeg': 'jpeg',
-        'image/webp': 'webp',
-        'image/apng': 'apng',
-        'image/jp2': 'jp2',
-        'image/jpx': 'jpx',
-        'image/jpm': 'jpm',
-        'image/bmp': 'bmp',
-        'image/svg+xml': 'svg',
+        "image/png": "png",
+        "image/jpeg": "jpeg",
+        "image/webp": "webp",
+        "image/apng": "apng",
+        "image/jp2": "jp2",
+        "image/jpx": "jpx",
+        "image/jpm": "jpm",
+        "image/bmp": "bmp",
+        "image/svg+xml": "svg",
     }
 
-    return lookup.get(mime, 'unk')
+    return lookup.get(mime, "unk")
 
 
-for i, page in enumerate(manifest['page']):
+for i, page in enumerate(manifest["page"]):
     p = f"{i + 1}/{total}"
 
-    pid, src = page.get('pid'), page.get('src')
+    pid, src = page.get("pid"), page.get("src")
     if not src:
         logging.error(f"[{p}] Skipped: download link for {pid} is missing…")
         continue
@@ -218,7 +226,7 @@ for i, page in enumerate(manifest['page']):
 
         ext = mime_to_ext(mimeType)
         filename = f"{pid}.{ext}"
-        with open(filename, 'wb') as file:
+        with open(filename, "wb") as file:
             file.write(buf)
 
         page_files.append(filename)
@@ -229,8 +237,8 @@ for i, page in enumerate(manifest['page']):
 
     time.sleep(GOOGLE_PAGE_DOWNLOAD_PACER)  # Be gentle with Google Play Books
 
-with open('pages.txt', 'w') as pages_file:
-    pages_file.write('\n'.join(page_files))
+with open("pages.txt", "w") as pages_file:
+    pages_file.write("\n".join(page_files))
 
 logging.info(
     f'Finished. The pages that got successfully downloaded can be found in "{book_dir}".')
--- play_book_pdf_tool/play_book_pdf_tool.py
+++ play_book_pdf_tool/play_book_pdf_tool.py
@@ -18,9 +18,10 @@ logging.getLogger().setLevel(logging.INFO)
 
 
 @click.command()
-@click.argument('book-base-path',
-                type=click.Path(exists=True, file_okay=False, dir_okay=True, writable=True, readable=True,
-                                path_type=pathlib.Path))
+@click.argument(
+    "book-base-path",
+    type=click.Path(exists=True, file_okay=False, dir_okay=True, writable=True, readable=True, path_type=pathlib.Path),
+)
 def pdf_generate(book_base_path: pathlib.Path):
     """Build a PDF from the Google Play Book pages located in the directory BOOK-BASE-PATH.
 
@@ -30,17 +31,18 @@ def pdf_generate(book_base_path: pathlib.Path):
     """
 
     try:
-        with open(f'{book_base_path}/manifest.json') as f_manifest:
+        with open(f"{book_base_path}/manifest.json") as f_manifest:
             manifest = json.load(f_manifest)
     except FileNotFoundError:
-        logging.error(
-            f"Couldn't find [{f'{book_base_path}/manifest.json'}]! Aborting...")
+        logging.error(f"Couldn't find [{f'{book_base_path}/manifest.json'}]! Aborting...")
         raise
 
-    pages_filename = pathlib.Path(book_base_path / 'pages.txt').read_text(encoding='UTF-8').splitlines()
+    pages_filename = pathlib.Path(book_base_path / "pages.txt").read_text(encoding="UTF-8").splitlines()
 
-    if manifest.get('is_right_to_left'):
-        logging.info(f"the manifest indicates that the book pages are ordered right to left. We will swap the order of the pages so that they show correctly in the PDF.")
+    if manifest.get("is_right_to_left"):
+        logging.info(
+            "the manifest indicates that the book pages are ordered right to left. We will swap the order of the pages so that they show correctly in the PDF."
+        )
 
         front = _.head(pages_filename)
         back = _.last(pages_filename)
@@ -51,12 +53,12 @@ def pdf_generate(book_base_path: pathlib.Path):
 
     page_paths = list(map(lambda fn: str(book_base_path / fn), pages_filename))
 
-    print(f'Merging {len(page_paths)} pages... (this can take a long time)')
+    print(f"Merging {len(page_paths)} pages... (this can take a long time)")
 
-    tmp_pdf = book_base_path / 'book-tmp.pdf'
+    tmp_pdf = book_base_path / "book-tmp.pdf"
     create_pdf(page_paths, tmp_pdf)
 
-    print('Adding the metadata and the table of contents...')
+    print("Adding the metadata and the table of contents...")
     with Pdf.open(tmp_pdf) as pdf:
         add_metadata(book_base_path, pdf)
         add_toc(book_base_path, pdf)
@@ -73,7 +75,7 @@ def pdf_generate(book_base_path: pathlib.Path):
 def create_pdf(image_paths, output_pdf_path):
     for path in image_paths:
         if os.path.getsize(path) == 0:
-            raise f'image at path [{path}] is empty'
+            raise f"image at path [{path}] is empty"
         # test-read a byte from it so that we can abort early in case
         # we cannot read data from the file
         with open(path, "rb") as im:
@@ -85,7 +87,7 @@ def create_pdf(image_paths, output_pdf_path):
 
 def generate_output_pdf_filename(base_path):
     try:
-        with open(f'{base_path}/manifest.json') as f_manifest:
+        with open(f"{base_path}/manifest.json") as f_manifest:
             manifest = json.load(f_manifest)
     except FileNotFoundError:
         logging.error(f"Couldn't find [{f'{base_path}/manifest.json'}]! Aborting...")
@@ -93,17 +95,17 @@ def generate_output_pdf_filename(base_path):
 
     m = _(manifest)
 
-    title = m.get('metadata.title').apply(html.unescape).value()
-    year = m.get('metadata.pub_date').split('.').head().value()
-    authors = m.get('metadata.authors').apply(html.unescape).value()
+    title = m.get("metadata.title").apply(html.unescape).value()
+    year = m.get("metadata.pub_date").split(".").head().value()
+    authors = m.get("metadata.authors").apply(html.unescape).value()
 
-    filename = f'{title} ({year}) — {authors}'
-    return to_valid_filename(filename) + '.pdf'
+    filename = f"{title} ({year}) — {authors}"
+    return to_valid_filename(filename) + ".pdf"
 
 
 def add_metadata(base_path, pdf):
     try:
-        with open(f'{base_path}/manifest.json') as f_manifest:
+        with open(f"{base_path}/manifest.json") as f_manifest:
             manifest = json.load(f_manifest)
     except FileNotFoundError:
         logging.error(f"Couldn't find [{f'{base_path}/manifest.json'}]! Aborting...")
@@ -112,31 +114,31 @@ def add_metadata(base_path, pdf):
     with pdf.open_metadata() as pdf_metadata:
         m = _(manifest)
 
-        pdf_metadata['dc:title'] = m.get('metadata.title').apply(html.unescape).value()
+        pdf_metadata["dc:title"] = m.get("metadata.title").apply(html.unescape).value()
 
-        if authors := m.get('metadata.authors').value():
-            pdf_metadata['dc:creator'] = [html.unescape(author).strip() for author in
-                                          authors.split(',')]
+        if authors := m.get("metadata.authors").value():
+            pdf_metadata["dc:creator"] = [html.unescape(author).strip() for author in authors.split(",")]
 
-        if publisher := m.get('metadata.publisher').value():
-            pdf_metadata['dc:publisher'] = [publisher]
+        if publisher := m.get("metadata.publisher").value():
+            pdf_metadata["dc:publisher"] = [publisher]
 
-        if pub_date := m.get('metadata.pub_date').value():
-            xmp_date = pub_date.replace('.', '-', 3)
+        if pub_date := m.get("metadata.pub_date").value():
+            xmp_date = pub_date.replace(".", "-", 3)
 
             if validate_xmp_date(xmp_date):
-                pdf_metadata['xmp:CreateDate'] = xmp_date
+                pdf_metadata["xmp:CreateDate"] = xmp_date
             else:
                 logging.warning(
-                    f"Invalid xmp:CreateDate format '{xmp_date}'. Metadata will not include 'xmp:CreateDate'. See https://developer.adobe.com/xmp/docs/XMPNamespaces/XMPDataTypes/#date")
+                    f"Invalid xmp:CreateDate format '{xmp_date}'. Metadata will not include 'xmp:CreateDate'. See https://developer.adobe.com/xmp/docs/XMPNamespaces/XMPDataTypes/#date"
+                )
         # Unused properties:
         #   metadata
         #       .num_pages
         #       .preview
         #       .volume_id
 
-        if language := m.get('language').value():
-            pdf_metadata['dc:language'] = [language]
+        if language := m.get("language").value():
+            pdf_metadata["dc:language"] = [language]
 
         # Unused properties:
         #   volume_version
@@ -157,29 +159,29 @@ def add_metadata(base_path, pdf):
 
 def add_toc(book_base_path, pdf):
     try:
-        with open(book_base_path / 'toc.json') as f_toc:
+        with open(book_base_path / "toc.json") as f_toc:
             toc = json.load(f_toc)
     except FileNotFoundError:
         logging.error("Couldn't find toc.json! Falling back to manifest.json. The outline structure will be flat...")
         try:
-            with open(book_base_path / 'manifest.json') as f_toc:
-                toc = json.load(f_toc)['toc_entry']
+            with open(book_base_path / "manifest.json") as f_toc:
+                toc = json.load(f_toc)["toc_entry"]
         except FileNotFoundError:
             logging.error("Couldn't find manifest.json either! Aborting...")
             raise
 
     if toc is None or len(toc) == 0:
-        raise 'No table of contents or no entries'
+        raise "No table of contents or no entries"
 
     with pdf.open_outline() as pdf_outline:
         pdf_outline.root.clear()
 
         for toc_item in toc:
-            label, depth, page_index = itemgetter('label', 'depth', 'page_index')(toc_item)
+            label, depth, page_index = itemgetter("label", "depth", "page_index")(toc_item)
             label = html.unescape(label)
 
             parent = pdf_outline.root
-            for _ in range(depth):
+            for __ in range(depth):
                 parent = parent[-1].children
 
             outline_item = OutlineItem(html.unescape(label), page_index)
@@ -213,5 +215,5 @@ def validate_xmp_date(date_str):
     return False
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pdf_generate()
